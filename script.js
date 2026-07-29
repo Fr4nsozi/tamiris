ddd1c1d9f8f7b4b682285bb000de42359b49f27e
Fr4nsozi <matheusfransozi@hotmail.com>
2026-07-29 20:07:44 -0300

    :tada: Commit Inicial

---
+++ script.js
@@ -0,0 +1,10 @@
+const link = document.getElementById("contato2");
+const image = document.getElementById("calendar");
+
+link.addEventListener("mouseenter", () => {
+    image.src = "img/calendar-hover.svg";
+});
+
+link.addEventListener("mouseleave", () => {
+    image.src ="img/calendar.svg";
+});
